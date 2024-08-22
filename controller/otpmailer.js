117c8c3cf0cacb83c60d67482b1e364581f1068c
Willsen Yogi <willsenyp21@gmail.com>
2024-08-22 20:54:21 +0700

    created otp controller

---
+++ controller/otpmailer.js
@@ -0,0 +1,21 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER, 
+    pass: process.env.EMAIL_PASS, 
+  },
+});
+const sendMail = (to, subject, text) => {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: to,
+    subject: subject,
+    text: text,
+  };
+
+  return transporter.sendMail(mailOptions);
+};
+
+module.exports = sendMail;
