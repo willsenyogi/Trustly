d031f062e1adf8ffa9be6e99eb170e78a3bc6b7a
Willsen Yogi <willsenyp21@gmail.com>
2024-08-24 14:30:45 +0700

    added functionality to reset password through otp code

--- routes/router.js
+++ routes/router.js
@@ -732,5 +732,99 @@ router.post("/api/resetpassword/submit", async (req, res) => {
   res.redirect("/profile");
 });
 
+router.get("/forgotpassword/pr", async (req, res) => {
+  try {
+    if (req.isAuthenticated()) {
+      const loggedUser = await User.findById(req.session.passport.user);
+      const otp =  Math.floor(100000 + Math.random() * 900000);
+      req.session.otpfgpr = otp;
+      req.session.save();
+      sendMail(loggedUser.email, "OTP Code", "Your OTP code is: " + otp);
+      res.render("forgotpassword-pr", {
+        title: "Trustly - Forgot Password",
+        showHeader: false,
+        showFooter: false,
+        showDashboardNav: false,
+        loggedUser,
+        fgprMessages: req.flash("fgprMessages"),
+      });
+    } else {
+      res.redirect("/login");
+    } 
+  } catch (error) {
+    console.error("Error loading forgot password page:", error);
+    res.status(500).send("An error occurred while loading the forgot password page.");
+  }
+});
+
+router.post("/api/forgotpassword/pr/submit", async (req, res) => {
+  try {
+    if (req.isAuthenticated()) {
+      const loggedUser = await User.findById(req.session.passport.user);
+      const { otpcodepr } = req.body;
+      if (String(req.session.otpfgpr) !== otpcodepr) {
+        req.flash("fgprMessages", ["Incorrect OTP code."]);
+        return res.redirect("/forgotpassword/pr");
+      } else {
+        res.redirect("/forgotpassword/pr/otpconfirmed/true");
+      }
+    } else {
+      res.redirect("/login");
+    }
+  } catch (error) {
+    console.error("Error changing password:", error);
+    res.status(500).send("An error occurred while changing the password.");
+  }
+});
+
+router.get("/forgotpassword/pr/otpconfirmed/true", async (req, res) => {
+  try {
+    if (req.isAuthenticated()) {
+      const loggedUser = await User.findById(req.session.passport.user);
+      res.render("forgotpassword-pr-otpconfirmed", {
+        title: "Trustly - Forgot Password",
+        showHeader: false,
+        showFooter: false,
+        showDashboardNav: false,
+        loggedUser,
+        fgpr2Messages: req.flash("fgpr2Messages"),
+      });
+    } else {
+      res.redirect("/login");
+    }
+  } catch (error) {
+    console.error("Error loading forgot password page:", error);
+    res.status(500).send("An error occurred while loading the forgot password page.");
+  }
+})
+
+router.post("/api/forgotpassword/pr/otpconfirmed/true/change", async (req, res) => {
+  try {
+    if (req.isAuthenticated()) {
+      const loggedUser = await User.findById(req.session.passport.user);
+      const { newpassword } = req.body;
+      if (!newpassword) {
+        req.flash("fgpr2Messages", ["Please enter a new password."]);
+        return res.redirect("/forgotpassword/pr/otpconfirmed/true");
+      }
+
+      if (newpassword.length < 8) {
+        req.flash("fgpr2Messages", ["Password must be at least 8 characters long."]);
+        return res.redirect("/forgotpassword/pr/otpconfirmed/true");
+      }
+      loggedUser.password = await bcrypt.hash(newpassword, 10);
+      await loggedUser.save();
+      req.flash("fgpr2Messages", ["Password changed successfully."]);
+      req.session.otpfgpr = null;
+      req.session.save();
+      res.redirect("/dashboard");
+    } else {
+      res.redirect("/login");
+    }
+  } catch (error) {
+    console.error("Error changing password:", error);
+    res.status(500).send("An error occurred while changing the password.");
+  }
+});
 
 module.exports = router;
