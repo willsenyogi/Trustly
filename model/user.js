2fa32d2c7bc64f16f06145be3cd20c2eb9496d3f
Willsen Yogi <willsenyp21@gmail.com>
2024-09-29 16:27:14 +0700

    revised balance data type

--- model/user.js
+++ model/user.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema({
         type: Number
     },
     balance: {
-        type: Number,
+        type: BigInt,
         default: 20000000
     },
     savingsTitle: {
